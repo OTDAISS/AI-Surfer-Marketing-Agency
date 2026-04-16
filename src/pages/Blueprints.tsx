be350ff15e088a1c958af4710058fc8f25e2a203
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-16 15:53:33 -0400

    Create Blueprints.tsx

---
+++ src/pages/Blueprints.tsx
@@ -0,0 +1,8 @@
+export default function Blueprints() {
+  return (
+    <div className="page">
+      <h1>Blueprints</h1>
+      <p>Architectural visions and system designs for your cinematic brand.</p>
+    </div>
+  )
+}
