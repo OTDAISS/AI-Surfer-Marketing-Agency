039d79564a4271988be01bea506d5ae78ac797a1
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-16 15:56:47 -0400

    Create Services.tsx

---
+++ src/pages/Services.tsx
@@ -0,0 +1,8 @@
+export default function Services() {
+  return (
+    <div className="page">
+      <h1>Services</h1>
+      <p>Professional offerings powered by Ocean Tide Drop AI Surfer.</p>
+    </div>
+  )
+}
