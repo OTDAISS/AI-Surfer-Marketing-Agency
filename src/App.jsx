e10621558aea5fbdabbe894b4bfb0ae468b72f35
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-17 16:21:05 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,31 @@ export default function App() {
   return (
     <div className="app-container">
       <Outlet />
-    </div>
+    </div>import { useState, useEffect } from 'react'
+import { supabase } from './utils/supabase'
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    async function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  )
+}
   );
 }
