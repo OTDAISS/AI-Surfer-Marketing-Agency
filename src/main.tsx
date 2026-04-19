22eb8fdb772549793fc3ee483fd373fbc27cebaa
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-19 11:54:10 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// src/main.jsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PricingOverview from './pages/pricing'
+import ProductPage from './pages/pricing/[slug]'
+import ThankYou from './pages/thank-you'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/pricing" element={<PricingOverview />} />
+      <Route path="/pricing/:slug" element={<ProductPage />} />
+      <Route path="/thank-you" element={<ThankYou />} />
+    </Routes>
+  </BrowserRouter>
+)
